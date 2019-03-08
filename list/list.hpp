89a92339cba14b0acd2526329e4a9e889ded0e63
JunrQ <137556741@qq.com>
2019-03-08 08:54:02 +0800

    update code:

---
+++ list/list.hpp
@@ -0,0 +1,41 @@
+
+// ListNode
+template <typename T>
+struct ListNode {
+  T data;
+  ListNode<T>* pred;
+  ListNode<T>* succ;
+
+  ListNode() {}
+  ListNode (T e, ListNode<T>* p=nullptr, ListNode<T>* s=nullptr)
+    : data(e), pred(p), succ(s) {}
+  
+  ListNode<T>* insertAsPred(T const& e);
+  ListNode<T>* insertAsSucc(T const& e);
+};
+
+// List
+template <typename T>
+class List {
+private:
+  int _size;
+  ListNode<T>* header;
+  ListNode<T>* trailer;
+
+protected:
+  void init();
+  int clear();
+  void copyNodes(ListNode<T>*, int);
+  void merge(ListNode<T>*, int, List<T>&, ListNode<T>*, int);
+
+  // mergesort
+  
+  // selectionSort
+
+  // insertionSort
+
+public:
+  List() { init(); }
+  
+};
+
