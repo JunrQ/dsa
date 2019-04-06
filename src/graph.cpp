1b3bf1d1b46e9db3cb0e256d598d8e4b560ae3b2
JunrQ <137556741@qq.com>
2019-04-07 00:08:12 +0800

    learn

--- src/graph.cpp
+++ src/graph.cpp
@@ -38,6 +38,52 @@ void Graph<Tv, Te>::bfs(int s) {
   } while (s != (v = (++v % n)));
 }
 
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::dfs(int s) {
+  reset();
+  int clock = 0;
+  int v = s;
+  do {
+    if (UNDISCOVERED == status(v)) DFS(v, clock);
+ } while (s != (v = (++v % n)));
+}
+
+template <typename Tv, typename Te>
+void Graph<Tv, Te>::DFS(int v, int& clock) {
+  dTime(v) = ++clock;
+  status(v) = DISCOVERED;
+  for (int u = firstNbr(v); -1 < u; u = nextNbr(v, u)) {
+    switch (status(u)) {
+      case UNDISCOVERED:
+        type(v, u) = TREE;
+        parent(u) = v;
+        DFS(u, clock);
+        break;
+      case DISCOVERED:
+        type(v, u) = BACKWARD;
+        break;
+      default:
+        type(v, u) = (dTime(v) < dTime(u)) ? FORWARD : CROSS;
+        break;
+    }
+  }
+  status(v) = VISITED;
+  fTime(v) = ++clock;
+}
+
+template <typename Tv, typename Te>
+Stack<Tv>* Graph<Tv, Te>::tSort(int s) {
+  reset();
+  int clock = 0;
+  int v = s;
+  Stack<Tv>* S = new Stack<Tv>;
+  do {
+    if (UNDISCOVERED == status(v)) {
+      while (!S->empty()) S->pop();
+      break;
+    }
+  }
+}
 
 
 // ----- Node -----
