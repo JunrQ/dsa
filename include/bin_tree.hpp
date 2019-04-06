c1f181e1348e46781b0c97395a3aafe72fac57e1
JunrQ <137556741@qq.com>
2019-04-06 15:04:04 +0800

    binary tree

--- include/bin_tree.hpp
+++ include/bin_tree.hpp
@@ -3,7 +3,6 @@
 namespace  dsa {
 
 // BinNode
-
 template <typename T>
 struct BinNode {
   T data;
@@ -31,33 +30,57 @@ struct BinNode {
   int size();
   BinNode* insertAsLC(T const &);
   BinNode* insertAsRC(T const &);
+
+  // in traverse succ
   BinNode* succ();
 
-  template <typename VST> void travLevel(VST&);
-  template <typename VST> void travPre(VST&);
-  template <typename VST> void travIn(VST&);
-  template <typename VST> void travPost(VST&);
+  template <typename VST> void travLevel(VST& visit) {
+    // TODO
+  }
+
+  // Recursive
+  template <typename VST> void travPre(VST& visit) {
+    visit(data);
+    if (lc) lc->travPre(visit);
+    if (rc) rc->travPre(visit);
+  }
+  template <typename VST> void travIn(VST& visit) {
+    if (lc) lc->travIn(visit);
+    visit(data);
+    if (rc) rc->travIn(visit);
+  }
+  template <typename VST> void travPost(VST& visit) {
+    if (lc) lc->travPost(visit);
+    if (rc) rc->travPost(visit);
+    visit(data);
+  }
 
   bool operator< (BinNode const& bn) {return data < bn.data;}
+  bool operator> (BinNode const& bn) {return data > bn.data;}
   bool operator== (BinNode const& bn) {return data == bn.data;}
+  bool operator!= (BinNode const& bn) {return data != bn.data;}
 
-  bool isRoot() { return !parent; }
-  bool isLChild() { return !isRoot() && this == parent->lc; }
-  bool isRChild() { return !isRoot() && this == parent->rc; }
-  bool hasParent() { return !isRoot(); }
-  bool hasLChild() { return lc ? true : false; }
-  bool hasRChild() { return rc ? true : false; }
-  bool hasChild() { return hasLChild() || hasRChild(); }
-  bool hasBothChild() { return hasLChild() && hasRChild(); }
-  bool isLeaf() { return !hasChild(); }
+  inline bool isRoot() { return !parent; }
+  inline bool isLChild() { return !isRoot() && this == parent->lc; }
+  inline bool isRChild() { return !isRoot() && this == parent->rc; }
+  inline bool hasParent() { return !isRoot(); }
+  inline bool hasLChild() { return lc ? true : false; }
+  inline bool hasRChild() { return rc ? true : false; }
+  inline bool hasChild() { return hasLChild() || hasRChild(); }
+  inline bool hasBothChild() { return hasLChild() && hasRChild(); }
+  inline bool isLeaf() { return !hasChild(); }
+  inline bool sibling() { return isLChild()? parent->rc : parent->lc; }
+  inline bool uncle() { return parent->isLChild()? parent->parent->rc : parent->parent->lc; }
+  inline BinNode* fromParentTo() {
+    return isRoot() ? this : (isLChild() ? parent->lc : parent->rc);
+  }
 
 }; // class BinNode
 
 
-
+// BinTree
 template <typename T>
 class BinTree {
-
 
 protected:
   int _size;
@@ -65,18 +88,28 @@ protected:
   virtual int updateHeight(BinNode<T>* x);
   void updateHeightAbove(BinNode<T>* x);
 public:
-  BinTree() : _size(0), _root(nullptr){}
+  BinTree() : _size(0), _root(nullptr) {}
   ~BinTree() {if (0 < _size) remove(_root);}
   int size() const {return _size;}
   bool empty() const {return !_root;}
   BinNode<T>* root() const {return _root;}
+  // Insert e as root
   BinNode<T>* insertAsRoot(T const& e);
+  // Insert e as x's child (not exist)
   BinNode<T>* insertAsLC(BinNode<T>* x, T const& e);
   BinNode<T>* insertAsRC(BinNode<T>* x, T const& e);
+  // Attach bintree as x's child
+  // If use pointer of BinTree, value of t is copied
+  // so can not be modified, but we want to set
+  // t to nullptr
   BinNode<T>* attachAsLC(BinNode<T>* x, BinTree* &t);
   BinNode<T>* attachAsRC(BinNode<T>* x, BinTree* &t);
+
+  // Remove node and its de
   int remove(BinNode<T>* x);
   BinTree* secede(BinNode<T>* x);
+
+  // traverse
   template <typename VST>
   void travLevel(VST& visit) {
     if (_root) _root->travLevel(visit);
@@ -93,14 +126,10 @@ public:
   void travPost(VST& visit) {
     if (_root) _root->travPost(visit);
   }
-
-  
-  
-
-
-
 }; // class BinTree
 
-} //  dsa
+
+
+} // namespace dsa
 
 
--- src/bin_tree.cpp
+++ src/bin_tree.cpp
@@ -1,10 +1,127 @@
 
 #include "bin_tree.hpp"
+#include <algorithm>
 
 namespace dsa {
 
+template <typename T>
+BinNode<T>* BinNode<T>::insertAsLC(T const& e) {
+  return lc = new BinNode(e, this);
+}
 
+template <typename T>
+BinNode<T>* BinNode<T>::insertAsRC(T const& e) {
+  return rc = new BinNode(e, this);
+}
 
+template <typename T>
+BinNode<T>* BinNode<T>::succ() {
+  BinNode* s = this;
+  if (rc) {
+    // right subtree
+    s = rc;
+    while (s->hasLChild()) s = s->lc;
+  } else {
+    // has no right child, so succ should
+    // be the lowest left subtree
+    while (s->isRChild()) s = s->parent;
+    s = s->parent;
+  }
+  return s;
+}
+
+template <typename T>
+int BinTree<T>::updateHeight(BinNode<T>* x) {
+  int lc_height = x->lc ? x->lc->height : 0;
+  int rc_height = x->rc ? x->rc->height : 0;
+  return x->height = 1 + max(lc_height, rc_height);
+}
+
+template <typename T>
+void BinTree<T>::updateHeightAbove(BinNode<T>* x) {
+  while (x) {
+    updateHeight(x);
+    x = x->parent;
+  }
+}
+
+template <typename T>
+BinNode<T>* BinTree<T>::insertAsRoot(T const& e) {
+  _size = 1;
+  return _root = new BinNode<T>(e);
+}
+
+template <typename T>
+BinNode<T>* BinTree<T>::insertAsLC(BinNode<T>* x, T const& e) {
+  _size++;
+  x->insertAsLC(e);
+  updateHeightAbove(x);
+  return x->lc;
+}
+
+template <typename T>
+BinNode<T>* BinTree<T>::insertAsRC(BinNode<T>* x, T const& e) {
+  _size++;
+  x->insertAsRC(e);
+  updateHeightAbove(x);
+  return x->lc;
+}
+
+template <typename T>
+BinNode<T>* BinTree<T>::attachAsLC(BinNode<T>* x, BinTree* &S) {
+  if (x->lc = S->_root) x->lc->parent = x; // if S->_root is nullptr
+  _size += S->_size;
+  updateHeightAbove(x);
+  S->_root = nullptr;
+  S->_size = 0;
+  // TODO release, delete S
+  S = nullptr;
+  return x;
+}
+
+template <typename T>
+BinNode<T>* BinTree<T>::attachAsRC(BinNode<T>* x, BinTree* &S) {
+  if (x->rc = S->_root) x->rc->parent = x; // if S->_root is nullptr
+  _size += S->_size;
+  updateHeightAbove(x);
+  S->_root = nullptr;
+  S->_size = 0;
+  // TODO release, delete S
+  S = nullptr;
+  return x;
+}
+
+template <typename T>
+int BinTree<T>::remove(BinNode<T>* x) {
+  x->fromParentTo() = nullptr;
+  updateHeightAbove(x->parent);
+  int n = removeAt(x);
+  _size -= n;
+  return n;
+}
+
+// non member
+template <typename T>
+static int removeAt(BinNode<T>* x) {
+  if (!x) return 0; //recursive base
+  int n = 1 + removeAt(x->lc) + removeAt(x_rc);
+  // TODO release data
+  delete x;
+  x = nullptr;
+  return n;
+}
+
+template <typename T>
+BinTree<T>* BinTree<T>::secede(BinNode<T>* x) {
+  x->fromParentTo() = nullptr;
+  updateHeightAbove(x->parent);
+  BinTree* S = new BinTree;
+  S->_root = x;
+  x->parent = nullptr;
+  S->_size = x->size();
+  _size = S->_size;
+  return S;
+}
 
 
 
