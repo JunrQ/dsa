973af713385e63723e529ebed42dedc5bcb0574b
junr <137556741@qq.com>
2020-07-16 23:10:19 +0800

    45

---
+++ problems/leetcode/0045.cpp
@@ -0,0 +1,33 @@
+/**
+ * 45. 跳跃游戏 II
+ * 
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ * 
+ * https://leetcode-cn.com/problems/jump-game-ii/
+ * 
+ */
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int maxPos = 0, n = nums.size(), end = 0, step = 0;
+        for (int i = 0; i < n - 1; ++i) {
+            if (maxPos >= i) {
+                maxPos = max(maxPos, i + nums[i]);
+                if (i == end) {
+                    end = maxPos;
+                    ++step;
+                }
+            }
+        }
+        return step;
+    }
+};
+
+
